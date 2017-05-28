7da85032950d688be0e1c077310f7b9fae0c723f
Denis Musin <TheLackyOne@gmail.com>
2017-05-28 07:14:17 +0300

    Commit

---
+++ app/src/main/java/android/learning/unit1/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package android.learning.unit1.geoquiz;
+
+/**
+ * Created by Denis on 27.05.2017.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+        mTextResId = textResId;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
